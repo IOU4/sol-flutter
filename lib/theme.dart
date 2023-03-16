be36c627ce73429b1231561e6c3cce317bfa625e
IOU4 <ouchaibimad000@gmail.com>
2023-03-16 15:52:20 +0100

    add themedata for cards

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 
-final ThemeData themeData = ThemeData(primarySwatch: Colors.red);
+final ThemeData themeData = ThemeData(
+    primarySwatch: Colors.red,
+    cardTheme: const CardTheme(
+        elevation: 10,
+        margin: EdgeInsets.all(4),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)))));
 // cb0331
